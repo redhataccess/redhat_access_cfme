9e592ef0f42553b97e7e0c56b1fbb416d2632560
Lindani Phiri <lphiri@redhat.com>
2017-10-24 21:16:43 -0400

    Try out 4.14.1

---
+++ scripts/asset_mod.sh
@@ -0,0 +1,4 @@
+sed -i -r 's/static/insights/g' vendor/assets/stylesheets/insights/in-prod.css
+sed -i -r 's/assets\/fonts/insights\/fonts/g' vendor/assets/stylesheets/insights/in-prod.css
+sed -i -r 's/assets\/images/insights/g' vendor/assets/stylesheets/insights/in-prod.css
+sed -i -r 's/.\/static\/images\/i_risk-of-change.svg/\/assets\/insights\/i_risk-of-change.svg/g' vendor/assets/javascripts/insights/insights.js
